3c10f452db2008cbe68cfc79b903a7370fac69db
Mohammed Niyas <nzmniyascse@gmail.com>
2018-09-22 04:21:00 +0000

    added example for go custom package

---
+++ mymath/mymath.go
@@ -0,0 +1,9 @@
+package mymath
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Substract(a, b int) int {
+	return a - b
+}
---
+++ packaging.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sample-app/mymath"
+)
+
+func main() {
+	fmt.Println(mymath.Substract(3,4))
+}
